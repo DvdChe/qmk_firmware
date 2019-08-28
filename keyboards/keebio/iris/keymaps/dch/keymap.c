ee3cf05d737066510f870cc26eccc017f7710359
David CHERRIERE <dcherriere@splio.com>
2019-08-28 17:37:05 +0200

    set new keymap

--- keyboards/keebio/iris/keymaps/dch/keymap.c
+++ keyboards/keebio/iris/keymaps/dch/keymap.c
@@ -24,9 +24,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LGUI, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,           KC_RSFT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+     KC_LGUI, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,           KC_RALT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LCTRL, LOWER,   KC_SPC,                   KC_ENT,  RAISE,   KC_RALT
+                                    KC_LCTRL, LOWER,   KC_SPC,                   KC_ENT,  KC_MINS,  RAISE
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
